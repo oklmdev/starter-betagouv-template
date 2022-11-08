a194637f2bc71902f433b117e7b1da2cfb3bf99c
Pierre A. Duchateau <pad@dabla.com>
2022-11-08 17:48:10 +0100

    Ajouter un preview DSFR sur la page admin (#4)
    * Use custom preview for landing page, with DSFR style
    
    * add build:cms to build
    
    * Remove test-repo as default
    
    * Fix error

--- src/pages/_components/Nl2Br.tsx
+++ src/pages/_components/Nl2Br.tsx
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
 
 export function Nl2Br(str: string) {
-  return str.split('\n').map((item, key) => (
-    <Fragment key={`key${key}`}>
-      {item}
-      <br />
-    </Fragment>
-  ));
+  return (
+    str &&
+    str.split('\n').map((item, key) => (
+      <Fragment key={`key${key}`}>
+        {item}
+        <br />
+      </Fragment>
+    ))
+  );
 }
