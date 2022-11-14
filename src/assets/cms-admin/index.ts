8c52ecacb46c38f24fe87e5ecaf3b31514ab97f2
Pierre A. Duchateau <pad@dabla.com>
2022-11-14 17:35:17 +0100

    Retirer registerPreviewStyle

--- src/assets/cms-admin/index.ts
+++ src/assets/cms-admin/index.ts
@@ -2,7 +2,6 @@ import CMS from 'netlify-cms';
 import { LandingPreview } from './LandingPreview';
 
 CMS.init();
-CMS.registerPreviewStyle('/dsfr.min.css');
 CMS.registerPreviewTemplate('landing', LandingPreview);
 
 export default {
